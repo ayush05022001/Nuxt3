1ff19f84a8ff337f37eef973057e99b8df844020
AYUSH SRIVASTAVA <56442013+ayush05022001@users.noreply.github.com>
2023-11-14 17:06:51 +0530

    Nested API Calls

---
+++ nested.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+const paragraphs = ref()
+
+const route = useRoute()
+
+const { data: page } = await useFetch(`/api/page/${route.params.slug}`)
+
+
+watch(page, (newPage)=>{
+    if (newPage.Id) {
+
+      useFetch(`/api/page/${newPage.Id}/paragraphs`).then((response)=>{
+         paragraphs.value  = response.data
+
+     })
+        
+    }
+}, {
+    deep: true,
+    immediate:true
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
